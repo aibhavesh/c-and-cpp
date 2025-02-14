e5fe99fa58d28b40902ff57a8fd583c6e5d5dc1b
bhavesh prajapat <aibhavesh27@gmail.com>
2025-02-15 00:35:54 +0530

    add some new

--- absolutevalue.c
+++ absolutevalue.c
@@ -6,7 +6,7 @@ int main()
 	scanf("%d",&x);
 	if(x<0){
 	y=x*(-1);
-	printf("absolute value of integer %d is %d ",x,y);
+	printf("absolute value of bhavesh integer %d is %d ",x,y);
 }else 
 printf("absolute value of integer %d is %d ",x,x);
 	return 0;
